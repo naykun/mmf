0c066c3963562f127ede9be5658347d5ea791c41
Vedanuj Goswami <vedanujg@gmail.com>
2020-09-07 21:09:39 -0700

    [refactor] Refactor MMF Transformer embeddings to support scriptability

--- mmf/models/transformers/base.py
+++ mmf/models/transformers/base.py
@@ -19,6 +19,9 @@ class BaseTransformerInput:
     segment_ids: Dict[str, Tensor]  # dict of segment/token type ids for all modalities
     masks: Dict[str, Tensor]  # dict of masks for all modalities
 
+    def __getitem__(self, item):
+        return getattr(self, item)
+
 
 @dataclass
 class BaseModalityConfigType:
