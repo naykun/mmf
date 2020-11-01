bac2c79d50dce20fd5eefaebaca34862331ae16f
naykun <yankun1138283845@foxmail.com>
2020-11-01 08:54:42 +0000

    add prediction support for localized narratives

--- mmf/datasets/builders/localized_narratives/caption_dataset.py
+++ mmf/datasets/builders/localized_narratives/caption_dataset.py
@@ -1,5 +1,5 @@
 from abc import ABC
-
+import torch
 
 from mmf.common.sample import Sample
 from mmf.common.typings import MMFDatasetConfigType
@@ -7,6 +7,7 @@ from mmf.datasets.builders.localized_narratives.database import (
     LocalizedNarrativesAnnotationDatabase, BboxAlignedLocalizedNarrativesAnnotationDatabase
 )
 from mmf.datasets.mmf_dataset import MMFDataset
+from mmf.utils.distributed import byte_tensor_to_object, object_to_byte_tensor
 
 
 class TracedCaptionLocalizedNarrativesDatasetMixin(ABC):
@@ -34,10 +35,23 @@ class TracedCaptionLocalizedNarrativesDatasetMixin(ABC):
             {"timed_caption": sample_info["timed_caption"], "bbox_attend_scores": image_info_0['bbox_attend_scores']})
         # should be a trace enhanced processor
         current_sample.update(processed_caption)
-        current_sample.image_id = sample_info["image_id"]
+        current_sample.image_id = object_to_byte_tensor(sample_info["image_id"])
         current_sample.feature_path = sample_info["feature_path"]
 
         return current_sample
+
+    def format_for_prediction(self, report):
+        captions = report.captions.tolist()
+        predictions = []
+
+        for idx, image_id in enumerate(report.image_id):
+            image_id = byte_tensor_to_object(image_id)
+            caption = self.caption_processor.id2tokens(captions[idx]).split()
+            if isinstance(image_id, torch.Tensor):
+                image_id = image_id.item()
+            predictions.append({"image_id": image_id, "caption": caption})
+
+        return predictions
 
 
 class TracedCaptionLocalizedNarrativesDataset(
